2a468a615e21451e1b70ab725560a91a12db9298
Alakhdeep singh <54256747+Alakhdeepsingh@users.noreply.github.com>
2022-02-14 21:33:02 +0530

    payment

---
+++ src/About.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+let fixed = {
+  minHeight: "85vh",
+};
+let none = {
+  listStyle: "none",
+  fontSize: "30px",
+  marginTop: "10px",
+};
+let border = {
+  border: "solid 5px #345B63",
+
+  backgroundImage:
+    "linear-gradient(to right top, #93b5c6, #9fb9ca, #aabdcd, #b4c1d0, #bec6d2, #c4c9d4, #caccd6, #d0cfd8, #d5d1da, #dbd3db, #e0d6db, #e4d8dc)",
+};
+
+export const About = () => {
+  return (
+    <div style={fixed}>
+      <div style={border}>
+        <p className="text-center">
+          <h3>We are Animal NGO </h3> <br />{" "}
+          <h6>
+            <ol style={none}>
+              <li>
+                The fancy jackets that you wear are not worth the life of those
+                innocent animals. Quit buying fur and leather, but a ban to it.
+              </li>
+              <li>
+                The world is their home as well, save the unsafe. Live and the
+                animals live by putting a stop to animal abuse.
+              </li>
+              <li>
+                Love those innocent creatures; don’t kill them to wear them.
+                Wear fake fur and leather for the sake of the animals. Your
+                fashion is not worth a life.
+              </li>
+              <li>
+                When you are looking for a pet, why not adopt an animal that is
+                waiting in a shelter for a home rather than purchasing them.
+              </li>
+              <li>
+                Saving one animal will not help in changing the world, but
+                saving that one animal will change that animal’s world.
+              </li>
+              <li>
+                The scientist has found evidence – through indirect ways as it
+                is not possible to observe another creature directly – in favour
+                that mammals and birds do see dreams when they are sleeping.
+              </li>
+              <li>
+                Wildlife is the greatest gift that Mother Nature has given us.
+                It is our responsibility to take to respect and protect what we
+                have been gifted. Many species, one Earth, one opportunity, one
+                future.
+              </li>
+              <li>
+                {" "}
+                By caging the innocent, you are cruel. Start caging the cruelty,
+                not the animals. Treat the speechless with kindness.
+              </li>
+            </ol>
+          </h6>
+          <br />
+
+        </p>
+      </div>
+    </div>
+  );
+};
---
+++ src/App.js
@@ -0,0 +1,454 @@
+import "./App.css";
+import logo from "./logo.svg";
+import { About } from "./About";
+import "bootstrap/dist/css/bootstrap.min.css";
+import * as ReactBootStrap from "react-bootstrap";
+import img1 from "./img1.jpeg";
+import img2 from "./img2.jpg";
+import img3 from "./img3.jpg";
+import img4 from "./L.png";
+import img6 from "./f.png";
+import img7 from "./img7.png";
+import img8 from "./img8.png";
+import img9 from "./img9.png";
+import i1 from "./i1.jpg";
+import i3 from "./i3.jpg";
+import i2 from "./i2.jpg";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+let style = {
+  //width: "10px",
+  height: "650px",
+};
+//slider
+
+let light = {
+  color: "white",
+};
+//footer text
+
+let size = {
+  width: "70px",
+  height: "50px",
+  padding: "5px",
+};
+//contact us icons size
+
+let margin = {
+  marginTop: "20px",
+  marginBottom: "20px",
+};
+//contact us margin
+
+let margin1 = {
+  marginLeft: "90px",
+};
+//navbar donate button right left
+
+let margin2 = {
+  margin: "10px",
+  height:650,
+  width:20
+
+};
+//gapping between our vision and below three box
+
+let wow = {
+  paddingLeft: "20px",
+};
+//AnimalNGO space from the left in navabar
+
+let hey = {
+  padding: "100px",
+  
+};
+//padding of all the three box
+
+let hey1 = {
+  paddingRight: "100px",
+  paddingLeft: "10px",
+};
+//linkedin and github below animalngo.org
+
+let color = {
+  backgroundImage:
+    "linear-gradient(to bottom, #3333ff 0%, #ff99cc 100%)"
+};
+//background color
+
+
+
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div style={color} className="App">
+          <ReactBootStrap.Navbar bg="light" expand="lg">
+            <ReactBootStrap.Navbar.Brand as={Link} style={wow} to="/">
+              Animal NGO
+            </ReactBootStrap.Navbar.Brand>
+            <ReactBootStrap.Navbar.Toggle aria-controls="navbarScroll" />
+            <ReactBootStrap.Navbar.Collapse id="navbarScroll">
+              <ReactBootStrap.Nav
+                className="mr-auto my-2 my-lg-0"
+                style={{ maxHeight: "100px" }}
+                
+              >
+                <ReactBootStrap.Nav.Link as={Link} style={wow} to="/about">
+                  About
+                </ReactBootStrap.Nav.Link>
+              </ReactBootStrap.Nav>
+              <ReactBootStrap.Button
+                style={margin1}
+                className="float-right"
+                href="https://rzp.io/l/9qZJbg5"
+                variant="outline-success"
+              >
+                Donate
+              </ReactBootStrap.Button>
+            </ReactBootStrap.Navbar.Collapse>
+          </ReactBootStrap.Navbar>
+          <Switch>
+            <Route exact path="/">
+              {
+                <>
+                  <div>
+                    <ReactBootStrap.Carousel>
+                      <ReactBootStrap.Carousel.Item interval={1000}>
+                        <img
+                          style={style}
+                          className="w-100"
+                          src={img1}
+                          alt="First slide"
+                        />
+                        <ReactBootStrap.Carousel.Caption>
+                          <h3>Save Animals</h3>
+                          <p>
+                            Divided by intelligence, united by a heartbeat –
+                            Save animals.
+                          </p>
+                        </ReactBootStrap.Carousel.Caption>
+                      </ReactBootStrap.Carousel.Item>
+                      <ReactBootStrap.Carousel.Item interval={1000}>
+                        <img
+                          style={style}
+                          className="d-block w-100"
+                          src={img2}
+                          alt="Second slide"
+                        />
+                        <ReactBootStrap.Carousel.Caption>
+                          <h3>Stand for Animal Rights!</h3>
+                          <p>
+                            Be kind to animals or stop calling yourself human.
+                          </p>
+                        </ReactBootStrap.Carousel.Caption>
+                      </ReactBootStrap.Carousel.Item>
+                      <ReactBootStrap.Carousel.Item>
+                        <img
+                          style={style}
+                          className="d-block w-100"
+                          src={img3}
+                          alt="Third slide"
+                        />
+                        <ReactBootStrap.Carousel.Caption>
+                          <h3>Respect animals</h3>
+                          <p>The Earth will lose its soul without animals.</p>
+                        </ReactBootStrap.Carousel.Caption>
+                      </ReactBootStrap.Carousel.Item>
+                    </ReactBootStrap.Carousel>
+                  </div>
+
+
+
+                <div class="row" >
+                  <div class="col col-4" >
+
+<div>
+                  <ReactBootStrap.Modal.Dialog>
+                    <ReactBootStrap.Modal.Title style={wow}>
+                      Our Vision
+                    </ReactBootStrap.Modal.Title>
+
+                    <ReactBootStrap.Modal.Body>
+                      <p>
+                        Animal NGO is founded by visionaries who felt the need
+                        to build a free of cost medical facility for the stray
+                        animals where the best, most advanced, ethical and
+                        highly skilled medical care is readily available.
+                      </p>
+                    </ReactBootStrap.Modal.Body>
+
+                    <ReactBootStrap.Modal.Footer>
+                      <ReactBootStrap.Button
+                        className="btn btn-success"
+                        variant="primary"
+                        href="https://rzp.io/l/9qZJbg5"
+                      >
+                        Donate
+                      </ReactBootStrap.Button>
+                    </ReactBootStrap.Modal.Footer>
+                  </ReactBootStrap.Modal.Dialog>
+                  </div>
+                  </div>
+
+
+                  <div class="col col-4">
+
+                  <div>
+
+
+                  <ReactBootStrap.Modal.Dialog>
+                    <ReactBootStrap.Modal.Title style={wow}>
+                      Our Vision
+                    </ReactBootStrap.Modal.Title>
+
+                    <ReactBootStrap.Modal.Body>
+                      <p>
+                        Animal NGO is founded by visionaries who felt the need
+                        to build a free of cost medical facility for the stray
+                        animals where the best, most advanced, ethical and
+                        highly skilled medical care is readily available.
+                      </p>
+                    </ReactBootStrap.Modal.Body>
+
+                    <ReactBootStrap.Modal.Footer>
+                      <ReactBootStrap.Button
+                        className="btn btn-success"
+                        variant="primary"
+                        href="https://rzp.io/l/9qZJbg5"
+                      >
+                        Donate
+                      </ReactBootStrap.Button>
+                    </ReactBootStrap.Modal.Footer>
+                  </ReactBootStrap.Modal.Dialog>
+                  </div>
+</div>
+
+<div class="column col-4">
+
+
+<div>
+
+<ReactBootStrap.Modal.Dialog>
+  <ReactBootStrap.Modal.Title style={wow}>
+    Our Vision
+  </ReactBootStrap.Modal.Title>
+
+  <ReactBootStrap.Modal.Body>
+    <p>
+      Animal NGO is founded by visionaries who felt the need
+      to build a free of cost medical facility for the stray
+      animals where the best, most advanced, ethical and
+      highly skilled medical care is readily available.
+    </p>
+  </ReactBootStrap.Modal.Body>
+
+  <ReactBootStrap.Modal.Footer>
+    <ReactBootStrap.Button
+      className="btn btn-success"
+      variant="primary"
+      href="https://rzp.io/l/9qZJbg5"
+    >
+      Donate
+    </ReactBootStrap.Button>
+  </ReactBootStrap.Modal.Footer>
+</ReactBootStrap.Modal.Dialog>
+
+</div>
+                  </div>
+                  </div>
+
+
+                  <ReactBootStrap.CardGroup style={hey}>
+                    <ReactBootStrap.Card style={(hey, margin2)}>
+                      <ReactBootStrap.Card.Img variant="top" src={i3} />
+                      <ReactBootStrap.Card.Body>
+                        <ReactBootStrap.Card.Title>
+                          Critical Medical Care
+                        </ReactBootStrap.Card.Title>
+                        <ReactBootStrap.Card.Text>
+                          There are so many animals day in & day out that are
+                          subjected to cruelty, road rage and medical concerns.
+                          Karma Foundation works towards helping such animals
+                          either in our OPD or by admitting them into our
+                          facility. Our current set-up in Pune City is our pilot
+                          model. We plan to expand our horizon by working on
+                          building a larger unit for farm animals, a sanctuary
+                          for the old and discarded and a shelter home for the
+                          ones who cannot survive on the streets of the citya
+                          sanctuary for the old. stray animals at our OPD
+                          clinic.
+                        </ReactBootStrap.Card.Text>
+                        <ReactBootStrap.Button
+                        className="bottom"
+                          href="https://www.karmafoundation.ngo/"
+                          variant="primary"
+                        >
+                          Visit
+                        </ReactBootStrap.Button>
+                      </ReactBootStrap.Card.Body>
+                    </ReactBootStrap.Card>
+
+                    <ReactBootStrap.Card style={(hey, margin2)}>
+                      <ReactBootStrap.Card.Img variant="top" src={i1} />
+                      <ReactBootStrap.Card.Body>
+                        <ReactBootStrap.Card.Title>
+                          Animal Birth Control (ABC)
+                        </ReactBootStrap.Card.Title>
+                        <ReactBootStrap.Card.Text>
+                          Karma foundation truly believes that the best way to
+                          reduce human - animal conflict & animal – animal
+                          conflict is birth control. We conduct ABC surgeries
+                          each day at our facility & keep the animal for Post
+                          Operative care until they are fit to be released back
+                          where they were picked up from.Our current set-up in
+                          Pune City is our pilot model. We plan to expand our
+                          horizon by working on building a larger unit for farm
+                          animals, a sanctuary for the old and discarded and a
+                          shelter home for the ones who cannot survive on the
+                          streets of the city.
+                        </ReactBootStrap.Card.Text>
+                        <ReactBootStrap.Button
+                        className="bottom"
+                          href="https://www.karmafoundation.ngo/"
+                          variant="primary"
+                        >
+                          Visit
+                        </ReactBootStrap.Button>
+                      </ReactBootStrap.Card.Body>
+                    </ReactBootStrap.Card>
+
+                    <ReactBootStrap.Card style={(hey, margin2)}>
+                      <ReactBootStrap.Card.Img variant="top" src={i2} />
+                      <ReactBootStrap.Card.Body>
+                        <ReactBootStrap.Card.Title>
+                          Out Patient Department (OPD service)
+                        </ReactBootStrap.Card.Title>
+                        <ReactBootStrap.Card.Text>
+                          Our OPD is a facility that is responsible for
+                          providing prevention, cure, and alleviation of
+                          diseases and pain. Our OPD clinic is a fully
+                          functional facility which is equipped to handle
+                          critical emergencies. We offer free treatment for
+                          stray animals at our OPD clinic. Each day we attend to
+                          number of patients who are victims of cruelty, road
+                          accidents and who suffer from various illnesses. Our
+                          partner, Animal Care Clinic has been generous by
+                          extending their support towards our cause.
+                        </ReactBootStrap.Card.Text>
+                        <ReactBootStrap.Button
+                        className="bottom"
+                          variant="primary"
+                          href="https://www.karmafoundation.ngo/"
+                        >
+                          Visit
+                        </ReactBootStrap.Button>
+                      </ReactBootStrap.Card.Body>
+                    </ReactBootStrap.Card>
+                  </ReactBootStrap.CardGroup>
+
+                  {/* <section>
+                    <div className="text-center">
+                      <h2>Contact Us</h2>
+                      <div style={margin}>
+                        <a href="https://www.linkedin.com/signup">
+                          <img style={size} src={img4} />
+                        </a>
+                        <a style={size} href="https://www.facebook.com/">
+                          <img style={size} src={img7} />
+                        </a>
+                        <a href="https://twitter.com/?lang=en">
+                          <img style={size} src={img8} />
+                        </a>
+                        <a href="https://www.youtube.com/">
+                          <img style={size} src={img9} />
+                        </a>
+                      </div>
+                    </div>
+                  </section> */}
+                </>
+              }
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+          </Switch>
+
+          <footer className="bg-dark">
+            <div className="container-fluid py-5">
+              <div className="row text-center">
+                <div className="col-md-4 ">
+                  {/* <hr className="light" style={light} /> */}
+                  <h5 style={light}>Contact Us</h5>
+                  {/* <hr className="light" style={light} /> */}
+                  <p style={light}>(+91) 9026776613</p>
+                  <p style={light}>alakhdeepsingh773@gmail.com</p>
+                </div>
+
+
+<div class="col-md-4" >
+          
+                    <div className="text-center">
+                      <h2 style={light}>Social Media</h2>
+                      <div style={margin}>
+                        <a href="https://www.linkedin.com/signup">
+                          <img style={size} src={img4} />
+                        </a>
+                        <a style={size} href="https://www.facebook.com/">
+                          <img style={size} src={img7} />
+                        </a>
+                        <a href="https://twitter.com/?lang=en">
+                          <img style={size} src={img8} />
+                        </a>
+                        <a href="https://www.youtube.com/">
+                          <img style={size} src={img9} />
+                        </a>
+                      </div>
+                    </div>
+                  
+</div>
+
+
+
+
+
+
+                <div className="col-md-4">
+                  {/* <hr className="light" style={light} /> */}
+                  <h5 style={light}>About</h5>
+                  {/* <hr className="light" style={light} /> */}
+                  <a href="https://www.pawsindia.org/">
+                    <p style={light}>Links</p>
+                  </a>
+                  <a href="https://www.scoopwhoop.com/ngos-for-animals/">
+                    <p style={light}>Other Links</p>
+                  </a>
+                </div>
+                <div className="col-12">
+                  <hr className="light-100" style={light} />
+                  <h5 style={light}>&copy; AnimalNGO.org</h5>
+                  <h6 style={light}>Developer Contact</h6>
+                  {/* <a
+                    style={hey1}
+                    href="https://www.linkedin.com/in/priyaanshu-mittra-9bb0961b9/"
+                  >
+                    Linkedin
+                  </a>
+                  <a style={hey1} href="https://github.com/mittrajpeg606">
+                    Github
+                  </a> */}
+                </div>
+                
+              </div>
+            </div>
+          </footer>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
